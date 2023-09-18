87e23df4a78b105b2bcb1355ffe05fcc60e3f837
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-17 22:40:44 -0700

    completed form fetch lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+    const input = document.querySelector("input#searchByID");
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+
+        console.log(data);
+      });
+
+    e.target.reset();
+    // e.target.children[1].value;
+    console.log(input.value);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
